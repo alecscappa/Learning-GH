0da6f1c07edbaf14043789d8284fc17f924779c9
Alec Scappa <alec.scappa@gmail.com>
2022-03-14 00:42:14 +1100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Alec Scappa on 14/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
